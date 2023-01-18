2cdc195c91b2da7bf3c834e8a41c5ce5909d629d
Daemon <paakwesinunoo135@gmail.com>
2023-01-18 10:10:39 -0800

    completed crud

--- handlers/recipeHandler.js
+++ handlers/recipeHandler.js
@@ -1,44 +1,66 @@
 const Recipe = require("../models/recipeSchema");
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require("express-async-handler");
 
+const getRecipes = asyncHandler(async (req, res) => {
+  const foods = await Recipe.find({}).exec();
+  if (!foods.length) {
+    return res.status(404).json({ message: "No recipes found" });
+  } else {
+    res.status(200).json(foods);
+  }
+});
 
-const getRecipes = asyncHandler(
-    async (req,res) => {
-        const foods = await Recipe.find({}).exec()
-        if(!foods.length){
-            return res.status(404).json({message:"No recipes found"})
-        }else {
-            res.status(200).json(foods)
-        }
-    }
-)
+const createRecipes = asyncHandler(async (req, res) => {
+  const { foodName, image, ingredients, instructions, otherInfo } = req.body;
 
-const createRecipes =  asyncHandler(
-    async (req, res) => {
-        const { foodName, image, ingredients, instructions, otherInfo } = req.body;
-      
-        if (!foodName || !image || !ingredients || !instructions) {
-          res.status(400);
-          throw new Error("please add all fields");
-        }
-      
-        const recipe = await Recipe.create({
-          foodName,
-          image,
-          ingredients,
-          instructions,
-          otherInfo,
-      
-        })
-        if (recipe){
-          return res.status(201).json(recipe)
-        }else{
-          return res.status(400).json({message:"recipe creation failed"})
-        }
-      }
-)
+  if (!foodName || !image || !ingredients || !instructions) {
+    res.status(400);
+    throw new Error("please add all fields");
+  }
+
+  const recipe = await Recipe.create({
+    foodName,
+    image,
+    ingredients,
+    instructions,
+    otherInfo,
+  });
+  if (recipe) {
+    return res.status(201).json(recipe);
+  } else {
+    return res.status(400).json({ message: "recipe creation failed" });
+  }
+});
+
+const updateRecipe = asyncHandler(async (req, res) => {
+  const foodId = req.params.id;
+  const updatedRecipe = req.body;
+
+  const uprecipe = await Recipe.findByIdAndUpdate(foodId, updatedRecipe, { new: true });
+
+  if (!uprecipe) {
+    return res.status(404).json({ message: "Recipe not found" });
+  }
+
+  res.status(200).json(uprecipe);
+});
+
+const deleteRecipe = asyncHandler(async (req, res) => {
+  const foodId = req.params.id;
+
+  const recipe = await Recipe.findByIdAndDelete(foodId);
+
+  if (!recipe) {
+    return res.status(404).json({ message: "Recipe not found" });
+  }
+
+  res.status(200).json({ message: "Recipe deleted successfully" });
+});
+
 
 module.exports = {
-    getRecipes,
-    createRecipes
-}
+  getRecipes,
+  createRecipes,
+  updateRecipe,
+  deleteRecipe,
+};
--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
-const {createRecipes,getRecipes} = require('../handlers/recipeHandler')
+const {createRecipes,getRecipes,updateRecipe,deleteRecipe} = require('../handlers/recipeHandler')
 
 router.get('/',getRecipes)
 router.post('/',createRecipes)
+router.put('/:id',updateRecipe)
+router.delete('/:id',deleteRecipe)
 
 module.exports = router
